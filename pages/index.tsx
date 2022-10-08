083e4d7ecb567ca1882ce31a7702b5f764c26735
toke <toke8677@gmail.com>
2022-10-08 22:35:28 +0300

    Add feedback card

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import Highlights from "@/components/Home/Highlights"
 import FAQ from "@/components/Home/FAQ"
 import TermsCondensed from "@/components/Home/TermsCondensed"
 import Support from "@/components/Home/Support"
+import Feedback from "@/components/Home/Feedback"
 
 export default function Home() {
   return (
     <>
       <Landing />
       <Highlights />
+      <Feedback />
       <FAQ />
       <Support />
       <TermsCondensed />
