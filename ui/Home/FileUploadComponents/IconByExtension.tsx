58357d29b1c61d86919641cf21a933852f1a9c8d
toke <toke8677@gmail.com>
2022-11-09 17:39:20 +0200

    Add icon support for more file types

--- ui/Home/FileUploadComponents/IconByExtension.tsx
+++ ui/Home/FileUploadComponents/IconByExtension.tsx
@@ -1,7 +1,15 @@
-import { FaFile } from "react-icons/fa"
+import React from "react"
+import { FaFile, FaFileArchive, FaFileImage, FaImage, FaRegFileArchive } from "react-icons/fa"
 
-export default function IconByExtension(props: {
-    type: string
-}) {
-    return <FaFile />
-} 
+export default function IconByExtension(props: { type: string }) {
+    console.log(props)
+
+    switch (true) {
+        case props.type.includes("application"):
+            return <FaFileArchive />
+        case props.type.includes("image"):
+            return <FaFileImage />
+        default:
+            return <FaFile />
+    }
+}
