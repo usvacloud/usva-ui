8458bca1e5a6a32b7e7d321015bd3463bb0c15fa
toke <toke8677@gmail.com>
2022-11-13 15:43:15 +0200

    minify next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+    reactStrictMode: true,
+    swcMinify: true,
 }
-
-module.exports = nextConfig
